0144b57e29a4f83c80c8c90ebb793d8b437b5a1e
manosetro <manosetro@iti.gr>
2017-01-30 11:04:10 +0200

    Fix config to accept system variables

--- src/main/java/gr/iti/mklab/framework/common/domain/config/Configuration.java
+++ src/main/java/gr/iti/mklab/framework/common/domain/config/Configuration.java
@@ -3,6 +3,8 @@ package gr.iti.mklab.framework.common.domain.config;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.mongodb.morphia.annotations.Entity;
 
@@ -24,6 +26,8 @@ public class Configuration extends JSONable implements Iterable<String> {
 	 */
 	private static final long serialVersionUID = 5070483137103099259L;
 	
+	private static Pattern pattern = Pattern.compile("(\\Q${\\E)(.*)(\\Q}\\E)");
+	
 	public static final String CLASS_PATH = "Classpath";
 	
 	private Map<String, String> params = new HashMap<String, String>();
@@ -32,8 +36,15 @@ public class Configuration extends JSONable implements Iterable<String> {
 		return getParameter(name,null);
 	}
 	
-	public String getParameter(String name, String defaultValue){
+	public String getParameter(String name, String defaultValue){	
 		String value = params.get(name);
+		
+		Matcher matcher = pattern.matcher(value);
+		if(value != null && matcher.find()) {
+			value = matcher.group(2);
+			value = System.getProperty(value);
+		}
+		
 		return value == null ? defaultValue : value;
 	}
 	
